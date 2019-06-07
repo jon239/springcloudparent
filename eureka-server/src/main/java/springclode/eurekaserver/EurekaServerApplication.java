1d6822c25a8859d1a3ec96bf3927440134d83130
jon239 <2770566239@qq.com>
2019-06-07 17:24:25 +0800

    add

--- eureka-server/src/main/java/springclode/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/springclode/eurekaserver/EurekaServerApplication.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
-        System.out.println("Server start");
+        System.out.println("Server startaaa");
         SpringApplication.run(EurekaServerApplication.class, args);
     }
 
